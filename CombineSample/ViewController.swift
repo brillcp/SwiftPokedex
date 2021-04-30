09ceecb3e8311a508f0b46262c36f6c1d86cca70
brillcp <viktor.gidlof@storytel.com>
2021-04-30 10:13:55 +0200

    Clean up

--- CombineSample/ViewController.swift
+++ CombineSample/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  CombineSample
-//
-//  Created by Viktor Gidlöf on 2021-04-30.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
 }
-
