2dc575b271037b0553d9df08fdc3b1de7aaad157
brillcp <viktor.gidlof@storytel.com>
2021-04-30 13:14:23 +0200

    Fix network agent

--- CombineSample/CombineNetworking/NetworkAgent.swift
+++ CombineSample/CombineNetworking/NetworkAgent.swift
@@ -1,12 +1,23 @@
 import Foundation
 import Combine
 
-struct NetworkAgent {
-    func execute<T: Decodable>(_ request: URLRequest) -> AnyPublisher<T, Error> {
+final class NetworkAgent<T: Decodable> {
+    private var cancellables = Set<AnyCancellable>()
+    
+    func execute(_ request: URLRequest, _ completion: @escaping (Result<T, Error>) -> Swift.Void) {
         URLSession.shared.dataTaskPublisher(for: request)
             .tryMap { $0.data }
             .decode(type: T.self, decoder: JSONDecoder())
             .receive(on: DispatchQueue(label: "some.background.queue", qos: .background))
             .eraseToAnyPublisher()
+            .sink { completed in
+                switch completed {
+                case let .failure(error): completion(.failure(error))
+                case .finished: break
+                }
+            } receiveValue: { data in
+                completion(.success(data))
+            }
+            .store(in: &cancellables)
     }
 }
--- CombineSample/CombineNetworking/PokemonAPI/PokemonAPI.swift
+++ CombineSample/CombineNetworking/PokemonAPI/PokemonAPI.swift
@@ -1,11 +1,8 @@
 import Foundation
-import Combine
 
 struct PokemonAPI {
-    private static let agent = NetworkAgent()
+    private static let agent = NetworkAgent<PokemonResponse>()
     private static let baseURL = URL(string: "https://pokeapi.co/api/v2/")!
-    
-    private static var cancellable: AnyCancellable?
 
     enum PokemonType: String, CaseIterable {
         case electric, ground, flying, fire, water
@@ -14,16 +11,6 @@ struct PokemonAPI {
     static func requestPokemon(type: PokemonType, _ completion: @escaping (Result<PokemonResponse, Error>) -> Swift.Void) {
         let url = baseURL.appendingPathComponent("type/" + type.rawValue)
         let request = URLRequest(url: url)
-        
-        let publisher: AnyPublisher<PokemonResponse, Error> = agent.execute(request)
-
-        cancellable = publisher.sink { completed in
-            switch completed {
-            case .failure(let error): completion(.failure(error))
-            case .finished: break
-            }
-        } receiveValue: { response in
-            completion(.success(response))
-        }
+        agent.execute(request, completion)
     }
 }
