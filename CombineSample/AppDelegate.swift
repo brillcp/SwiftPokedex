5501d3037505d609d1be8166d90b1a22053c429a
brillcp <viktor.gidlof@storytel.com>
2021-04-30 17:22:50 +0200

    Omit return keyword

--- CombineSample/AppDelegate.swift
+++ CombineSample/AppDelegate.swift
@@ -4,7 +4,7 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+        true
     }
 
     // MARK: UISceneSession Lifecycle
