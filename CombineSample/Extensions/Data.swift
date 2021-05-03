acceabb27490010d45f86a58a18377da70d6936f
VG <viktor.gidlof@storytel.com>
2021-05-03 12:28:57 +0200

    Add space

--- CombineSample/Extensions/Data.swift
+++ CombineSample/Extensions/Data.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Data {
     var prettyJSON: String? {
         guard let object = try? JSONSerialization.jsonObject(with: self, options: []) else { return nil }
-        guard let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]) else { return nil}
+        guard let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]) else { return nil }
         return String(data: data, encoding: .utf8)
     }
 }
