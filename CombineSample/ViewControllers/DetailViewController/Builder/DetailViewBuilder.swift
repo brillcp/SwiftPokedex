ddf2688ae3373c67d61171a74d88a56cdcd96fd8
VG <viktor.gidlof@storytel.com>
2021-05-03 19:50:29 +0200

    Changed function argument names

--- CombineSample/ViewControllers/DetailViewController/Builder/DetailViewBuilder.swift
+++ CombineSample/ViewControllers/DetailViewController/Builder/DetailViewBuilder.swift
@@ -2,7 +2,7 @@ import UIKit
 
 final class DetailViewBuilder {
     
-    static func build(pokemon: Item, color: UIColor?) -> DetailViewController {
+    static func build(from pokemon: Item, withColor color: UIColor?) -> DetailViewController {
         let viewModel = DetailViewController.ViewModel(pokemon: pokemon, color: color)
         return DetailViewController(viewModel: viewModel)
     }
--- CombineSample/ViewControllers/PokedexViewController/Router/PokedexViewRouter.swift
+++ CombineSample/ViewControllers/PokedexViewController/Router/PokedexViewRouter.swift
@@ -9,7 +9,7 @@ final class PokedexRouter: PokedexRouterProtocol {
     weak var navigationController: UINavigationController?
     
     func routeToDetailView(pokemon: Item, color: UIColor?) {
-        let detailView = DetailViewBuilder.build(pokemon: pokemon, color: color)
+        let detailView = DetailViewBuilder.build(from: pokemon, withColor: color)
         navigationController?.pushViewController(detailView, animated: true)
     }
 }
