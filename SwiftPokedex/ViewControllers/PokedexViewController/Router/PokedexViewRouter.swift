814c3d4e5a3b07bf192bd9d734795baaa1d927e3
VG <viktor.gidlof@storytel.com>
2021-05-04 17:43:16 +0200

    Remove unused view controller

--- SwiftPokedex/ViewControllers/PokedexViewController/Router/PokedexViewRouter.swift
+++ SwiftPokedex/ViewControllers/PokedexViewController/Router/PokedexViewRouter.swift
@@ -20,28 +20,3 @@ final class PokedexRouter: PokedexRouterProtocol {
         navigationController?.pushViewController(detailView, animated: true)
     }
 }
-
-final class ViewController: UIViewController {
-    
-    private let pokemon: PokemonDetails
-    
-    init(pokemon: PokemonDetails) {
-        self.pokemon = pokemon
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let imageView = UIImageView(frame: view.frame)
-        imageView.contentMode = .scaleAspectFit
-        view.addSubview(imageView)
-        imageView.pinToSuperview()
-        
-        UIImage.load(from: pokemon.sprite.url) { image in
-            imageView.image = image
-        }
-    }
-}
