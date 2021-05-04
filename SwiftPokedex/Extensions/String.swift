3f0c8982fbcd7d24435192b05be47f34684f814e
VG <viktor.gidlof@storytel.com>
2021-05-04 18:25:29 +0200

    Change line height

--- SwiftPokedex/Extensions/String.swift
+++ SwiftPokedex/Extensions/String.swift
@@ -5,8 +5,21 @@
 //  Created by Viktor Gidlöf on 2021-05-04.
 //
 
-import Foundation
+import UIKit
 
 extension String {
     var cleaned: String { replacingOccurrences(of: "-", with: " ").capitalized }
+    
+    func lineHeight(_ height: CGFloat) -> NSAttributedString? {
+        var string = self
+        string = string.replacingOccurrences(of: "é", with: "e")
+        
+        let attributedString = NSMutableAttributedString(string: string)
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = height
+        attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+
+        return attributedString
+    }
 }
